78f4ee9e44cac13b8780eb71a0210a0ed1e2093f
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2018-02-28 12:59:31 +0100

    Updated.

--- setup.py
+++ setup.py
@@ -5,34 +5,44 @@ from distutils.core import setup
 setup(name = 'imagedata',
     packages = ['imagedata'],
     version = '1.0.0a1',
-    description = 'Read/write image data to file(s). Handles DICOM, Nifti, VTI and mhd.',
+    description = 'Read/write medical image data',
     author = 'Erling Andersen',
     author_email = 'Erling.Andersen@Helse-Bergen.NO',
-	url = 'http://www.helse-bergen.no', # Change this later
-	classifiers = [
-		"Programming Language :: Python :: 2.7",
-		"Programming Language :: Python :: 3",
-		"Development Status :: 3 - Alpha",
-		"Environment :: Other Environment",
+    url = 'https://github.com/erling6232/imagedata',
+    license = 'MIT',
+    keywords = 'dicom python medical imaging',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "Intended Audience :: Healthcare Industry",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
-		"Topic :: Scientific/Engineering :: Medical Science Apps.",
-		],
-	requires = ['pydicom',
-		'itk',
-		'vtk',
-		'nibabel',
-		'numpy',
-		'pathfinder',
-		],
-	long_description = """\
-	imagedata
-	==========
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+    ],
+    requires = ['pydicom',
+        'itk',
+        'vtk',
+        'nibabel',
+        'numpy',
+        'pathfinder',
+    ],
+    tests_require = ['tests'],
+    long_description = """\
+    # imagedata
 
-    Read/write image data to file(s). Handles DICOM, Nifti, VTI and mhd.
-	"""
+    Read/write medical image data.
+
+    Python library to read and write image data into numpy arrays.
+
+    Handles geometry information between the formats.
+
+    The following formats are included:
+    * DICOM
+    * Nifti
+    * ITK (MetaIO)
+    """
 )
