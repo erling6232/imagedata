f4678bf0cf34a6728d1b70a0d1e56c78fab5c54d
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2018-10-21 08:41:54 +0200

    Updated

--- imagedata/list_plugins.py
+++ imagedata/list_plugins.py
@@ -2,8 +2,21 @@
 
 import os
 import sys
+import logging
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+       '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+#logger.setLevel(logging.WARNING)
+#logger.setLevel(logging.ERROR)
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-print(sys.path)
+print("sys.path = \n{}".format(sys.path))
 
 from imagedata.formats.abstractplugin import AbstractPlugin
 import imagedata.formats as formats
@@ -11,20 +24,20 @@ import imagedata.formats as formats
 if __name__ == '__main__':
     print("__main__")
     plugins = formats.get_plugins_list()
-    print("Plugin Table 1")
-    print("==============")
+    print("Plugin Table List")
+    print("=================")
     for pname,ptype,pclass in plugins:
         print("%20s (%8s) %s" % (pname, ptype, pclass.description))
 
-    formats.add_plugin_dir('/hus/home/eran/src/image_data.py/packaging/imagedata/imagedata/formats')
-    plugins = formats.get_plugins_list()
-    print("Plugin Table 2")
-    print("==============")
-    for pname,ptype,pclass in plugins:
-        print("%20s (%8s) %s" % (pname, ptype, pclass.description))
+    #formats.add_plugin_dir('/hus/home/eran/src/image_data.py/packaging/imagedata/imagedata/formats')
+    #plugins = formats.get_plugins_list()
+    #print("Plugin Table 2")
+    #print("==============")
+    #for pname,ptype,pclass in plugins:
+    #    print("%20s (%8s) %s" % (pname, ptype, pclass.description))
 
     types = AbstractPlugin.__subclasses__()
-    print("Plugin Table 3")
-    print("==============")
+    print("Subclasses of AbstractPlugin")
+    print("============================")
     for plugin in types:
         print(plugin.__name__)
