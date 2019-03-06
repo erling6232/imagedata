5c97b9b829c09f13340ac821f113f24da9e20637
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2019-03-06 13:32:55 +0100

    Updated

--- tests/test_archive_file.py
+++ tests/test_archive_file.py
@@ -13,64 +13,62 @@ import argparse
 from .context import imagedata
 import imagedata.cmdline
 import imagedata.readdata
-import imagedata.formats
-from imagedata.series import Series
-from .compare_headers import compare_headers
+import imagedata.archives
+import imagedata.transports
 
-class test_file_archive_itk(unittest.TestCase):
+class test_archive_file(unittest.TestCase):
     def setUp(self):
         parser = argparse.ArgumentParser()
         imagedata.cmdline.add_argparse_options(parser)
 
-        #self.opts = parser.parse_args(['--template', 'tests/dicom/time/',
-        #        '--geometry', 'tests/dicom/time'])
-        #self.opts = parser.parse_args(['--of', 'itk', '--sort', 'tag'])
-        #self.opts = parser.parse_args(['--of', 'itk'])
         self.opts = parser.parse_args([])
         if len(self.opts.output_format) < 1: self.opts.output_format=['itk']
 
-        #self.hdr,self.si = imagedata.readdata.read(
-        #        ('tests/dicom/time/',),
-        #        imagedata.formats.INPUT_ORDER_TIME,
-        #        self.opts)
-
     def tearDown(self):
+        shutil.rmtree('ttaf', ignore_errors=True)
+
+    #@unittest.skip("skipping test_unknown_mimetype")
+    def test_unknown_mimetype(self):
         try:
-            shutil.rmtree('tti3')
-        except FileNotFoundError:
+            archive = imagedata.archives.find_mimetype_plugin(
+                'unknown',
+                'data',
+                'r')
+        except imagedata.archives.ArchivePluginNotFound:
             pass
+
+    #@unittest.skip("skipping test_mimetype")
+    def test_mimetype(self):
+        archive = imagedata.archives.find_mimetype_plugin(
+            '*',
+            'data',
+            'r')
+
+    #@unittest.skip("skipping test_unknown_url")
+    def test_unknown_url(self):
         try:
-            shutil.rmtree('tti4')
-        except FileNotFoundError:
+            archive = imagedata.archives.find_mimetype_plugin(
+                '*',
+                'unknown',
+                'r')
+        except imagedata.transports.RootIsNotDirectory:
             pass
 
-    #@unittest.skip("skipping test_read_single_file")
-    def test_read_single_file(self):
-        si1 = Series(
-            'data/itk/time/Image_00000.mha',
-            0,
-            self.opts)
-        self.assertEqual(si1.dtype, np.uint16)
-        self.assertEqual(si1.shape, (40, 192, 152))
-
-    #@unittest.skip("skipping test_read_two_files")
-    def test_read_two_files(self):
-        si1 = Series(
-            ['data/itk/time/Image_00000.mha',
-             'data/itk/time/Image_00001.mha'],
-            imagedata.formats.INPUT_ORDER_TIME,
-            self.opts)
-        self.assertEqual(si1.dtype, np.uint16)
-        self.assertEqual(si1.shape, (2, 40, 192, 152))
-
-    #@unittest.skip("skipping test_read_single_directory")
-    def test_read_single_directory(self):
-        si1 = Series(
-            'data/itk/time',
-            imagedata.formats.INPUT_ORDER_TIME,
-            self.opts)
-        self.assertEqual(si1.dtype, np.uint16)
-        self.assertEqual(si1.shape, (10, 40, 192, 152))
+    #@unittest.skip("skipping test_new_archive")
+    def test_new_archive(self):
+        archive = imagedata.archives.find_mimetype_plugin(
+            '*',
+            'ttaf',
+            'w')
+        with archive.open('test.txt', 'w') as f:
+            f.write(b'Hello world!')
+        read_archive = imagedata.archives.find_mimetype_plugin(
+            '*',
+            'ttaf',
+            'r')
+        with read_archive.open('test.txt', 'r') as f:
+            contents = f.read()
+        self.assertEqual(contents, b'Hello world!')
 
 if __name__ == '__main__':
     unittest.main()
