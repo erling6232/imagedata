897a1e444655886a3b9aa2164564a7deae8f0d73
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2020-03-17 13:21:17 +0100

    Initial release.

---
+++ imagedata/apps/Siemens/ROI.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+
+# ROI objects
+
+# Copyright (c) 2017-2018 Erling Andersen, Haukeland University Hospital, Bergen, Norway
+
+import logging
+import numpy as np
+from scipy import ndimage
+import math
+from abc import ABCMeta, abstractmethod, abstractproperty
+from draw_antialiased import draw_circle_mask, draw_polygon_mask
+
+class ROI(object, metaclass=ABCMeta):
+    """General ROI object."""
+
+    def __init__(self, label, stu_ins_uid, ser_ins_uid, sop_ins_uid):
+        object.__init__(self)
+        self.points        = None
+        self.points_matrix = None
+        self.slice         = None
+        self.label         = label
+        self.stu_ins_uid   = stu_ins_uid
+        self.ser_ins_uid   = ser_ins_uid
+        self.sop_ins_uid   = sop_ins_uid
+
+    @abstractmethod
+    def get_points_cm(self):
+        """Get ROI points as Numpy array in cm real coordinates
+
+        Output: np.array([points,3]) where each row is (z,y,x) in cm
+        """
+        pass
+
+    @abstractmethod
+    def get_points_matrix(self, hdr):
+        """Get ROI points as Numpy array in matrix coordinates
+
+        Input:  hdr: image header with transformation matrix
+        Output: np.array([points,3]) where each row is (z,y,x) in matrix coordinates
+        """
+        pass
+
+    def create_canvas(self, shape, mode=np.bool):
+        """Make a 2D [ny,nx] canvas
+
+        Input:
+        - shape: 4D [nt,nz,ny,nx] or 3D [nz,ny,nx] shape
+        - mode: np.bool for binary image, np.uint8 for 8-bit grayscale image
+        """
+        if len(shape) == 3:
+            nz,ny,nx = shape
+        elif len(shape) == 4:
+            nt,nz,ny,nx = shape
+        else:
+            raise ValueError("Shape has wrong length: {}".format(len(shape)))
+
+        return np.zeros((ny, nx), dtype=mode)
+
+    @abstractmethod
+    def draw_roi_on_canvas(self, canvas, colour=1, threshold=0.5, fill=False):
+        """Make a 2D mask [ny,nx] on canvas
+
+        Input:
+        - canvas: 2D [ny,nx]
+        - colour: mask color
+        - fill: whether to fill interior of ROI
+        - self.points_matrix: polygon points
+        - self.slice
+        Output:
+        - binary img of shape [ny,nx]
+        """
+        raise ValueError("Abstract ROI::draw_roi_on_canvas should not be called!")
+        pass
+
+    def draw_roi_on_numpy(self, mask, colour=1, threshold=0.5, fill=True):
+        """Draw the ROI on an existing numpy array
+
+        Input:
+        - mask: numpy array [nz,ny,nx]
+        - colour: mask colour (int)
+        - threshold: alpha blend (float)
+        - fill: whether to fill interior of ROI (bool)
+        Output:
+        - mask: modified numpy array [nz,ny,nx]
+        """
+        canvas = self.create_canvas(mask.shape)
+        self.draw_roi_on_canvas(canvas, colour=colour, threshold=threshold, fill=fill)
+        if not self.slice: print("ROI::draw_roi_on_numpy: no self.slice")
+        if self.slice:
+            mask[self.slice,:,:] = np.logical_or(mask[self.slice,:,:], canvas)
+        return mask
+
+    def verify_all_voxels_in_slice(self):
+        if self.points_matrix is None:
+            raise ValueError("Matrix points have not been calculated.")
+        iz,y,x = self.points_matrix[0]
+        for p in self.points_matrix:
+            z,y,x = p
+            if z != iz:
+                #print(self.points_matrix)
+                logging.debug("Point %d,%d,%d is not in slice %d." % (z,y,x,iz))
+                #raise ValueError("Point %d,%d,%d is not in slice %d." % (z,y,x,iz))
+        return iz
+
+    def transform_data_points_to_voxels(self, hdr, points):
+        voxels = []
+        for point in points:
+            t = hdr.getVoxelForPosition(np.array(point))
+            logging.debug("transform_data_points_to_voxels: {} -> {}".format(point,t))
+            z,y,x = hdr.getVoxelForPosition(np.array(point))
+            voxels.append((z,y,x))
+        return np.asarray(voxels)
+
+    def get_timepoint(self, hdr):
+        slice,tag,fname,im = hdr.search_sop_ins_uid(self.sop_ins_uid)
+        timeline = hdr.getTagList()
+        return timeline.index(tag)
+
+class PolygonROI(ROI):
+    """Polygon ROI object."""
+
+    def __init__(self, polygon, label, stu_ins_uid, ser_ins_uid, sop_ins_uid):
+        super(PolygonROI, self).__init__(label, stu_ins_uid, ser_ins_uid, sop_ins_uid)
+        self.points = polygon
+
+    def get_points_cm(self):
+        """Get ROI points as Numpy array in cm real coordinates
+
+        Output: np.array((points,3)) where each row is (z,y,x) in cm
+        """
+        return self.points
+
+    def get_points_matrix(self, hdr):
+        """Get ROI points as Numpy array in matrix coordinates
+
+        Input:  hdr: image header with transformation matrix
+        Output: np.array((points,3)) where each row is (z,y,x) in matrix coordinates
+        """
+        if self.points is None: _=self.get_points_cm()
+        if self.points_matrix is None:
+            self.points_matrix = self.transform_data_points_to_voxels(hdr, self.points)
+        return self.points_matrix
+
+    def draw_roi_on_canvas(self, canvas, colour=1, threshold=0.5, fill=False):
+        """Make a 2D mask [ny,nx] on canvas
+
+        Input:
+        - canvas: 2D [ny,nx]
+        - self.points_matrix: polygon points
+        - self.slice
+        - fill: whether to fill polygon interior
+        Output:
+        - canvas
+        """
+
+        #points_matrix = self.get_points_matrix(hdr)
+        #print("polygon: points cm    ", points)
+        #print("polygon: points matrix", polygon)
+        self.slice = self.verify_all_voxels_in_slice()
+
+        polygon2D = []
+        for p in self.points_matrix:
+            z,y,x = p
+            polygon2D.append((x,y))
+
+        draw_polygon_mask(canvas, polygon2D, colour, threshold, fill)
+
+        #pn = polygon2D[len(polygon2D)-1] # Close the polygon
+        #for p in polygon2D:
+        #    x1,y1 = pn
+        #    x2,y2 = p
+        #    draw_line_mask(canvas, x1, y1, x2, y2, fill, threshold)
+        #    pn = p
+        #canvas.save("/tmp/polygon.png", "PNG")
+
+        #if fill:
+        #    inside = point_in_polygon(canvas, polygon2D)
+        #    canvas = canvas + inside * fill
+
+class EllipseROI(ROI):
+    """Ellipse ROI object."""
+
+    def __init__(self, centre, angles, thickness, label, stu_ins_uid, ser_ins_uid, sop_ins_uid):
+        super(EllipseROI, self).__init__(label, stu_ins_uid, ser_ins_uid, sop_ins_uid)
+        assert centre.shape==(1,3), "Wrong shape of centre: " + str(centre.shape)
+        self.centre_cm = centre
+        self.angles_cm = angles
+        self.thickness_cm = thickness
+        angle1=angles[1]
+        self.radius_cm = math.sqrt(angle1[0]*angle1[0]+angle1[1]*angle1[1]+angle1[2]*angle1[2])
+
+    def get_points_cm(self):
+        """Get ROI points as Numpy array in cm real coordinates
+
+        Output: np.array((points,3)) where each row is (z,y,x) in cm
+        """
+        if self.points is None:
+            self.points = np.zeros([25,3])
+            #print("get_points_cm: self.points.shape", self.points.shape)
+            #print("get_points_cm: self.centre_cm.shape", self.centre_cm.shape)
+            #print("get_points_cm: self.centre_cm", self.centre_cm)
+            self.points[0,:] = self.centre_cm[0,:]
+            i = 1
+            for angle in np.arange(0.0, 2*math.pi, math.pi/12):
+                self.points[i,:] = self.centre_cm + self.radius_cm*np.array((math.sin(angle),0,math.cos(angle)))
+                i += 1
+            #self.points[1,:] = self.centre_cm - self.radius_cm*np.array((0,0,1))
+            #self.points[2,:] = self.centre_cm + self.radius_cm*np.array((0,0,1))
+            #self.points[3,:] = self.centre_cm - self.radius_cm*np.array((1,0,0))
+            #self.points[4,:] = self.centre_cm + self.radius_cm*np.array((1,0,0))
+        return self.points
+
+    def get_points_matrix(self, hdr):
+        """Get ROI points as Numpy array in matrix coordinates
+
+        Input:  hdr: image header with transformation matrix
+        Output: np.array((points,3)) where each row is (z,y,x) in matrix coordinates
+        """
+        if self.points is None: _=self.get_points_cm()
+        if self.points_matrix is None:
+            self.points_matrix = self.transform_data_points_to_voxels(hdr, self.points)
+            self.slice = self.verify_all_voxels_in_slice()
+            adjacent_cm = self.centre_cm + self.radius_cm*np.array((0,0,1))
+            adjacent_matrix = self.transform_data_points_to_voxels(hdr, adjacent_cm)
+            #print("self.points_matrix", self.points_matrix)
+            #print("self.points_matrix[0]", self.points_matrix[0])
+            #print("self.points_matrix[0][1]", self.points_matrix[0][1])
+            #print("adjacent_matrix", adjacent_matrix)
+            #print("adjacent_matrix[0][1]", adjacent_matrix[0][1])
+            self.radius_matrix = abs(adjacent_matrix[0][1] - self.points_matrix[0][1])
+            #print("radius_matrix", self.radius_matrix)
+        return self.points_matrix
+
+    def draw_roi_on_canvas(self, canvas, colour=1, threshold=0.5, fill=False):
+        """Make a 2D mask [ny,nx] on canvas
+
+        Input:
+        - canvas: 2D [ny,nx]
+        - fill: fill color
+        - self.points_matrix: polygon points
+        - self.slice
+        Output:
+        - canvas
+        """
+
+        self.slice = self.verify_all_voxels_in_slice()
+
+        centre = self.points_matrix[0]
+        yc,xc = centre[1],centre[2]
+        #radius = self.calculate_radius_matrix()
+        radius = self.radius_matrix
+        print("EllipseROI::draw_roi_on_canvas xc,yc,radius", xc,yc,radius)
+
+        draw_circle_mask(canvas, xc, yc, radius, colour, threshold, fill=fill)
+        #canvas.save("/tmp/ellipse.png", "PNG")
+
+    def calculate_radius_matrix(self):
+        """Calculate ellipse radius in matrix coordinates.
+        """
+        adjacent = self.centre_cm + self.radius_cm*np.array((0,0,1))
+        radius = abs(self.points[0,2]-adjacent[0,2])
+        return radius
---
+++ imagedata/apps/Siemens/evidence2mask.py
@@ -0,0 +1,294 @@
+"""Read Siemens Syngo.VIA Basic Reading
+"""
+
+# Copyright (c) 2017-2020 Erling Andersen, Haukeland University Hospital, Bergen, Norway
+
+import warnings, math, sys, os
+import numpy as np
+import logging
+import scipy.cluster.vq
+import matplotlib.pyplot as plt
+#import bitarray
+import scipy.misc
+from ROI import PolygonROI, EllipseROI
+
+"""
+def save_rois(rois, filename):
+    f = open("/home/root/x.txt", "w")
+    for roi in rois:
+        for p in roi.get_points_cm():
+            save_point(f, p)
+    f.close()
+
+def save_point(f, x):
+    f.write("%9.4f %9.4f %9.4f\n" % (x[0], x[1], x[2]))
+
+def save_x(x):
+    f = open("/home/root/xx.txt", "a")
+    f.write("%9.4f %9.4f %9.4f\n" % (x[0], x[1], x[2]))
+    f.close()
+
+def scan_local_options():
+    global separate_laterality
+
+    separate = "combined"
+    i = 0
+    while i < len(sys.argv):
+        if sys.argv[i] == "-laterality":
+            del sys.argv[i]
+            separate = sys.argv[i]
+            del sys.argv[i]
+        else:
+            i += 1
+    if separate not in ("combined", "separate"):
+        print("Unknown laterality argument: {}".format(separate))
+        sys.exit(1)
+    separate_laterality = (separate == "separate")
+"""
+
+def xyz_to_zyx(polygon):
+    """Swap (x,y,z) with (z,y,x) for each point
+    Input:
+    - polygon: np.array shape (n,3)
+    Output:
+    - rearranged np.array shape (n,3)
+    """
+    assert polygon.ndim == 2, "Array ndim should be 2, not {}".format(polygon.ndim)
+    newPolygon = np.zeros_like(polygon)
+    for i in range(polygon.shape[0]):
+        x,y,z = polygon[i,:]
+        newPolygon[i,:] = z,y,x
+    return newPolygon
+
+def evidence2roi(im, separate_laterality=False, uid_table=None, content={}):
+    # def get_rois(im, separate_laterality=False, uid_table=None, content={}):
+
+    """
+    overrows = im[(0x6000, 0x0010)].value
+    overcols = im[(0x6000, 0x0011)].value
+    overkey=(0x6000, 0x3000)
+    overlay=im[overkey].value
+
+    a=bitarray.bitarray(overlay, endian='little')
+
+    while len(a) < len(overlay)*8:
+            a.append(0)
+
+    c = np.array(a.tolist())
+    c = c.astype(np.uint8)
+    #c = c.reshape((overcols, overrows))
+    c = c.reshape((overrows, overcols))
+    """
+
+    #print("get_rois: {}".format(im.InstanceNumber))
+    #content = {}
+    rois = []	# List of ROI tuples
+
+    seqkey = (0x0029, 0x1102)
+    if seqkey in im:
+        presentationSQ = im[seqkey]
+    else:
+        seqkey = (0x0029, 0x1202)
+        if seqkey not in im:
+            logging.debug("get_rois: no presentation sequence (0x0029, 0x1202)")
+            return (rois, content)
+        presentationSQ = im[seqkey]
+
+    try:
+        measurement = presentationSQ[0][(0x0029, 0x10a7)]
+    except:
+        logging.debug("get_rois: no measurement sequence (0x0029, 0x10a7)")
+        return (rois, content)
+
+    findings = measurement[0][(0x0029, 0x1031)]
+
+    contentSQ = presentationSQ[0][(0x0029,0x10a9)]
+    content['label'] = contentSQ[0][(0x0070,0x0080)].value
+    content['description'] = contentSQ[0][(0x0070,0x0081)].value
+    content['date'   ] = contentSQ[0][(0x0070,0x0082)].value
+    content['time'   ] = contentSQ[0][(0x0070,0x0083)].value
+    content['creator'] = presentationSQ[0][(0x0070,0x0084)].value
+    #print("get_rois: creator: {}".format(content['creator']))
+
+    for finding in findings:
+        referenced_frame_seq = finding[(0x0029, 0x109a)][0]
+        referenced_syngo_uid = referenced_frame_seq[(0x0029, 0x1038)].value
+        if uid_table:
+            stu_ins_uid = uid_table[referenced_syngo_uid[0]]
+            ser_ins_uid = uid_table[referenced_syngo_uid[1]]
+            sop_ins_uid = uid_table[referenced_syngo_uid[2]]
+        else:
+            stu_ins_uid = referenced_syngo_uid[0]
+            ser_ins_uid = referenced_syngo_uid[1]
+            sop_ins_uid = referenced_syngo_uid[2]
+        sop_cla_uid = referenced_syngo_uid[3] # '1.2.840.10008.5.1.4.1.1.4'
+        # ROI number
+        meas_appl_number = finding[(0x0029, 0x1035)].value
+        roi_type_value   = finding[(0x0029, 0x1032)].value.strip()
+        try:
+            roi_name         = finding[(0x0029, 0x1030)].value
+        except KeyError:
+            roi_name         = 'NONAME'
+        logging.info("Finding: {} {} {}".format(content['creator'], roi_name, roi_type_value))
+        if roi_type_value == 'PolygonApplication3D' or roi_type_value == 'FreehandApplication3D':
+            output = finding[(0x0029,0x1096)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            polygon = meas_data_points.reshape((meas_data_points.size//3, 3))
+            logging.debug("XYZ: {}".format(polygon[0]))
+            polygon = xyz_to_zyx(polygon)
+            logging.debug("ZYX: {}".format(polygon[0]))
+
+            rois.append(PolygonROI(polygon, roi_name, stu_ins_uid, ser_ins_uid, sop_ins_uid))
+            logging.debug('ROI {}: {} points'.format(meas_appl_number, len(polygon)//3))
+            #print('ROI SOPInsUID: {}'.format(sop_ins_uid))
+        elif roi_type_value == 'EllipseApplication3D':
+            # Ellipsis centre
+            output = finding[(0x0029,0x1096)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            centre = meas_data_points.reshape((meas_data_points.size//3, 3))
+            centre = xyz_to_zyx(centre)
+            #print("ellipse centre:", centre)
+            # Ellipsis angles
+            output = finding[(0x0029,0x1097)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            angles = meas_data_points.reshape((meas_data_points.size//3, 3))
+            # Ellipsis thickness
+            output = finding[(0x0029,0x1099)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            thickness = meas_data_points
+
+            rois.append(EllipseROI(centre, angles, thickness, roi_name, stu_ins_uid, ser_ins_uid, sop_ins_uid))
+        elif roi_type_value == 'StandaloneTextApplication3D':
+            logging.warning("Standalone Text ROI not implemented.")
+            pass
+        elif roi_type_value == 'DistanceLineApplication3D':
+            logging.warning("DistanceLineApplication3D ROI not implemented.")
+            pass
+        else:
+            raise ValueError("ROI type %s not implemented." % roi_type_value)
+
+    return (rois, content)
+
+def make_mask_in_slice(roi_type, hdr, points, shape):
+    # Make a 3D mask [nz,ny,nx]
+
+    if roi_type == 'polygon':
+        # polygon = [(x1,y1),(x2,y2),...] or [x1,y1,x2,y2,...]
+        # width = ?
+        # height = ?
+
+        for p in points:
+            save_x(p)
+        polygon = transform_data_points_to_voxels(hdr, points)
+        points_matrix = roi.get_points_matrix(hdr)
+        """
+        print("polygon: points cm    {}".format(points))
+        print("polygon: points matrix", polygon)
+        """
+        slice = verify_all_voxels_in_slice(points_matrix)
+
+        mask = np.zeros(shape[1:], dtype=np.bool)
+
+        polygon2D = []
+        for p in points_matrix:
+            z,y,x = p
+            polygon2D.append((x,y))
+
+        img = Image.new('L', (shape[3], shape[2]), 0)
+        ImageDraw.Draw(img).polygon(polygon2D, outline=0, fill=1)
+    elif roi_type == 'ellipse':
+        #print("make_mask_in_slice: points", len(points), points)
+        (centre_cm, angles, thickness) = points
+        centre = transform_data_points_to_voxels(hdr, centre_cm)[0]
+        #print("make_mask_in_slice: centre (cm)", centre_cm)
+        #print("make_mask_in_slice: centre     ", centre)
+        slice = centre[0]
+        #print("make_mask_in_slice: slice ", slice)
+        mask = np.zeros(shape[1:], dtype=np.bool)
+
+        #print("make_mask_in_slice: angles", angles.shape, angles)
+        #print("make_mask_in_slice: angles", transform_data_points_to_voxels(hdr, angles))
+
+        angle1=angles[1]
+        radius_cm = math.sqrt(angle1[0]*angle1[0]+angle1[1]*angle1[1]+angle1[2]*angle1[2])
+        #print("make_mask_in_slice: radius_cm", radius_cm)
+        adjacent_cm = centre_cm + np.array((0,0,radius_cm))
+        save_x((centre_cm + np.array((0,0,radius_cm)))[0])
+        save_x((centre_cm - np.array((0,0,radius_cm)))[0])
+        save_x((centre_cm + np.array((radius_cm,0,0)))[0])
+        save_x((centre_cm - np.array((radius_cm,0,0)))[0])
+        #print("make_mask_in_slice: adjacent_cm", adjacent_cm)
+        adjacent = transform_data_points_to_voxels(hdr, adjacent_cm)[0]
+        #print("make_mask_in_slice: adjacent ", adjacent)
+        radius = abs(centre[1]-adjacent[1])
+        #print("make_mask_in_slice: radius   ", radius)
+
+        """
+        ellipse2D = []
+        for p in ellipse3D:
+            z,y,x = p
+            ellipse2D.append((x,y))
+        print("make_mask_in_slice: ellipse2D {}".format(ellipse2D))
+        """
+
+        yc,xc = centre[1],centre[2]
+        ellipse2D = (xc-radius, yc-radius, xc+radius, yc+radius)
+        img = Image.new('L', (shape[3], shape[2]), 0)
+        ImageDraw.Draw(img).ellipse(ellipse2D, outline=0, fill=1)
+    mask[slice,:,:] = np.array(img)
+    return mask
+
+def transform_data_points_to_voxels(hdr, meas_data_points):
+    polygon = []
+    for point in meas_data_points:
+        x,y,z = hdr.getVoxelForPosition(point)
+        polygon.append((z,y,x))
+    return polygon
+
+#def evidence2roi(im, separate_laterality=False, uid_table=None, content={}):
+#    #tg,fname,im = reading.DicomHeaderDict[0][0]
+#
+#    # Extract ROIs from syngo.via object
+#    rois,content = get_rois(im, separate_laterality, uid_table, content)
+#
+#    save_rois(rois, '/home/root/x.txt')
+#
+#    return rois,content
+
+'''
+def roi2mask(mask, content, roi, hdr, si, separate_laterality=False):
+    """Convert each roi into mask.
+    """
+
+    timeline = hdr.getTagList()
+
+    # Create masks for each laterality
+    for laterality in labelled_rois.keys():
+        if not laterality in mask:
+            # Create mask by building on one slice at a time
+            mask[laterality] = np.zeros(si.shape[1:], dtype=np.bool)
+            # mask[laterality] = np.zeros(si.shape[1:], dtype=np.uint8)
+        for key in labelled_rois[laterality].keys():
+            roi_type, meas_data_points, stu_ins_uid, ser_ins_uid, sop_ins_uid = labelled_rois[laterality][key]
+            try:
+                slice,tag,fname,im = hdr.search_sop_ins_uid(sop_ins_uid)
+            except:
+                continue
+            #print("SOPInsUID: %s" % sop_ins_uid)
+            print("  Laterality: %s, slice: %d, tag %s, timepoint %d" % (laterality, slice, tag, timeline.index(tag)))
+            print("  Date %s, time %s, creator %s" % (content['date'], content['time'], content['creator']))
+            mask[laterality] = np.logical_or(mask[laterality], make_mask_in_slice(roi_type, hdr, meas_data_points, si.shape))
+            content['timepoint'] = timeline.index(tag)
+'''
