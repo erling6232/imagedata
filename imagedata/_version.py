89d5a3fd38587c1d4168d3565a734c4b6a0b3c7c
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2020-11-04 15:21:50 +0100

    New version

--- imagedata/_version.py
+++ imagedata/_version.py
@@ -1,11 +1,11 @@
 """
-Provides . version information.
+Provides imagedata version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update --patch imagedata` to change this file.
+# Use `python -m incremental.update imagedata` to change this file.
 
 from incremental import Version
 
-__version__ = Version('.', 1, 1, 28)
+__version__ = Version('imagedata', 1, 1, 29)
 __all__ = ["__version__"]
