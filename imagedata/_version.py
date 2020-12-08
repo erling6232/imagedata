069208697de22df89d2fb1922b4dcbdb3d28adaa
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2020-12-08 14:44:34 +0100

    Updated

--- imagedata/_version.py
+++ imagedata/_version.py
@@ -7,5 +7,5 @@ Provides imagedata version information.
 
 from incremental import Version
 
-__version__ = Version('imagedata', 1, 1, 30)
+__version__ = Version('imagedata', 1, 1, 31)
 __all__ = ["__version__"]
