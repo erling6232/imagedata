bb46e97222e43022303f95519b2caace797ea843
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-02-24 14:40:52 +0100

    Simplified version number storage.

--- imagedata/__init__.py
+++ imagedata/__init__.py
@@ -1,12 +1,18 @@
 """imagedata"""
 
-import imagedata.formats
+# import imagedata.formats
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-import importlib.metadata
-__version__ = importlib.metadata.version('imagedata')
+try:
+    import importlib.metadata
+    __version__ = importlib.metadata.version('imagedata')
+except Exception:
+    import imagedata as _
+    from os.path import join
+    with open(join(_.__path__[0],"..","VERSION.txt"), 'r') as fh:
+        __version__ = fh.readline()
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
 __email__ = 'Erling.Andersen@Helse-Bergen.NO'
