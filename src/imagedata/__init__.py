c89133dd0fe1729f1ece46c4396509dd0fda3b56
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-03-04 14:41:27 +0100

    Strip version number.

--- src/imagedata/__init__.py
+++ src/imagedata/__init__.py
@@ -12,7 +12,7 @@ except Exception:
     import imagedata as _
     from os.path import join
     with open(join(_.__path__[0],"..","..","VERSION.txt"), 'r') as fh:
-        __version__ = fh.readline()
+        __version__ = fh.readline().strip()
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
 __email__ = 'Erling.Andersen@Helse-Bergen.NO'
