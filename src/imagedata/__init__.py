acddf45adeea30c123e9b6a573859e02d199e344
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-03-18 13:40:33 +0100

    Use importlib_metadata when importlib.metadata is not available.

--- src/imagedata/__init__.py
+++ src/imagedata/__init__.py
@@ -8,10 +8,13 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 try:
     import importlib.metadata
     __version__ = importlib.metadata.version('imagedata')
+except ModuleNotFoundError:
+    import importlib_metadata
+    __version__ = importlib_metadata.version('imagedata')
 except Exception:
     import imagedata as _
     from os.path import join
-    with open(join(_.__path__[0],"..","..","VERSION.txt"), 'r') as fh:
+    with open(join(_.__path__[0],"..","VERSION.txt"), 'r') as fh:
         __version__ = fh.readline().strip()
 
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
