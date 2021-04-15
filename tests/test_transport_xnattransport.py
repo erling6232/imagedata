93b3db6ccfced5a8ca9b99ab04a768ac5a20f9e2
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-04-15 14:29:25 +0200

    Do not really test xnat transfer since that would require access to an xnat server.

--- tests/test_transport_xnattransport.py
+++ tests/test_transport_xnattransport.py
@@ -1,13 +1,15 @@
 import unittest
-from imagedata.series import Series
+# from imagedata.series import Series
 
 
 class TestXNATTransport(unittest.TestCase):
 
     def test_something(self):
-        si1 = Series('xnat://xnat.medtek.haukeland.no/NyreHaugesund/TK/fPACS_E00184/5')
-        print(si1)
-        print(si1.shape)
+        # si1 = Series('xnat://xnat.server/project/subject/experiment/scan')
+        # si1.seriesNumber = 9999
+        # si1.seriesDescription = 'tttt'
+        # si1.write('xnat://xnat.server/project/subject/experiment')
+        pass
 
 
 if __name__ == '__main__':
