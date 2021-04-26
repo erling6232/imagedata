d5ddfff3cf02ab4df9dfd2fa0981fbb28209908b
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-04-26 09:12:54 +0200

    Updated plugin to read and write from XNAT archive.

--- src/imagedata/transports/xnattransport.py
+++ src/imagedata/transports/xnattransport.py
@@ -6,9 +6,11 @@
 import os
 import os.path
 import io
+import fnmatch
 import logging
 import shutil
 import tempfile
+import urllib
 import xnat
 from imagedata.transports.abstracttransport import AbstractTransport
 
@@ -20,7 +22,7 @@ class XnatTransport(AbstractTransport):
     name = "xnat"
     description = "Read and write files in xnat database."
     authors = "Erling Andersen"
-    version = "1.0.0"
+    version = "2.0.0"
     url = "www.helse-bergen.no"
     schemes = ["xnat"]
 
@@ -33,10 +35,13 @@ class XnatTransport(AbstractTransport):
         self.netloc = netloc
         self.opts = opts
         logging.debug("XnatTransport __init__ root: {}".format(root))
+        root_split = root.split('/')
         try:
-            project, subject, experiment = root.split('/')[1:4]
-        except ValueError:
+            project = root_split[1]
+        except IndexError:
             raise ValueError('Too few values in URL {}'.format(root))
+        subject = root_split[2] if len(root_split) > 2 else None
+        experiment = root_split[3] if len(root_split) > 3 else None
         self.__mode = mode
         self.__local = False
         self.__must_upload = False
@@ -44,13 +49,18 @@ class XnatTransport(AbstractTransport):
 
         self.__session = xnat.connect('https://'+self.netloc, verify=False)
         logging.debug("XnatTransport __init__ session: {}".format(self.__session))
-        self.__project = self.__session.projects[project]
+        self.__project = self.__session.projects[project] if project is not None else None
+        self.root = '/' + project
         logging.debug("XnatTransport __init__ project: {}".format(self.__project))
 
-        self.__subject = self.__project.subjects[subject]
-        logging.debug("Subject: {}".format(self.__subject.label))
-        self.__experiment = self.__subject.experiments[experiment]
-        logging.debug("Experiment: {}".format(experiment))
+        self.__subject = self.__project.subjects[subject] if subject is not None else None
+        if subject is not None:
+            self.root += '/' + subject
+            logging.debug("Subject: {}".format(self.__subject.label))
+        self.__experiment = self.__subject.experiments[experiment] if experiment is not None else None
+        if experiment is not None:
+            self.root += '/' + experiment
+            logging.debug("Experiment: {}".format(experiment))
 
     def close(self):
         """Close the transport
@@ -74,11 +84,52 @@ class XnatTransport(AbstractTransport):
         Return:
         - tuples of (root, dirs, files) 
         """
-        scan_id = top.split('/')[4]
+        if top[0] != '/':
+            # Add self.root to relative tree top
+            top = self.root + '/' + top
+        url_tuple = urllib.parse.urlsplit(top)
+        url = url_tuple.path.split('/')
+        if len(url) < 3:
+            raise ValueError('Too few terms in directory tree {}'.format(top))
+        elif len(url) == 3:
+            # Walk the subject list
+            subject_search = url[2]
+            for id in self.__project.subjects:
+                subject = self.__project.subjects[id]
+                if fnmatch.fnmatch(subject.label, subject_search):
+                    yield '/{}'.format(self.__project.id), [subject.label], []
+        elif len(url) == 4:
+            # Walk the experiment list
+            subject_id, experiment_search = url[2:4]
+            subject = self.__project.subjects[subject_id]
+            experiments = self._get_experiments(subject, experiment_search)
+            for experiment in experiments:
+                yield '/{}/{}'.format(self.__project.id, subject_id), [experiment], []
+        elif len(url) == 5:
+            # Walk the scan list
+            subject_id, experiment_search, scan_search = url[2:5]
+            subject = self.__project.subjects[subject_id]
+            experiments = self._get_experiments(subject, experiment_search)
+            for experiment_label in experiments:
+                experiment = subject.experiments[experiment_label]
+                scans = self._get_scans(experiment, scan_search)
+                yield '/{}/{}/{}'.format(self.__project.id, subject_id, experiment_label), scans, []
 
-        scan = self.__experiment.scans[scan_id]
-        logging.debug("Scan: {}".format(scan))
-        return [(top, scan_id, scan.files)]
+    def _get_experiments(self, subject, search):
+        experiments = []
+        for id in subject.experiments:
+            experiment = subject.experiments[id]
+            if fnmatch.fnmatch(experiment.label, search):
+                experiments.append(experiment.label)
+        return experiments
+
+    def _get_scans(self, experiment, search):
+        scans = []
+        for id in experiment.scans:
+            scan = experiment.scans[id]
+            if scan.quality == 'usable' and fnmatch.fnmatch(scan.type, search):
+                scans.append(scan.id)
+        return scans
 
     def isfile(self, path):
         """Return True if path is an existing regular file.
@@ -90,7 +141,21 @@ class XnatTransport(AbstractTransport):
         """
         if mode[0] == 'r' and not self.__local:
             scan_id = path.split('/')[4]
-            scan = self.__experiment.scans[scan_id]
+            if scan_id in self.__experiment.scans:
+                scans = [self.__experiment.scans[scan_id]]
+            else:
+                scans = []
+                for id in self.__experiment.scans:
+                    scan = self.__experiment.scans[id]
+                    if scan.quality == 'usable' and fnmatch.fnmatch(scan.type, scan_id):
+                        scans.append(scan)
+            if len(scans) == 0:
+                raise FileNotFoundError('Scan {} not found.'.format(scan_id))
+            elif len(scans) > 1:
+                raise ValueError('Too many scans match search "{}": {}'.format(
+                    scan_id, scans
+                ))
+            scan = scans[0]
             self.__tmpdir = tempfile.mkdtemp()
             if scan.quality == 'usable':
                 # scan.download_dir(self.__tmpdir)
