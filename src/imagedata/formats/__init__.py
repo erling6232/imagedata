a4f15919d07794acdb1cb879f65b98b4d88cfa9a
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2021-10-18 15:46:20 +0200

    Added utility function get_size(obj).

--- src/imagedata/formats/__init__.py
+++ src/imagedata/formats/__init__.py
@@ -5,6 +5,7 @@ Standard plugins provides support for DICOM and Nifti image file formats.
 
 # Copyright (c) 2013-2018 Erling Andersen, Haukeland University Hospital, Bergen, Norway
 
+import sys
 import logging
 import numpy as np
 
@@ -180,6 +181,27 @@ def shape_to_str(shape):
         return "{}".format(shape[0])
     else:
         raise ValueError("Unknown shape")
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
 
 
 # noinspection PyGlobalUndefined
