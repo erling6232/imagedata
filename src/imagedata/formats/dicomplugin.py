62b760a8269a7fcc657a4c87e32216be2f711df5
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2022-12-16 13:48:41 +0100

    DICOMPlugin.extractDicomAttributes(): Extract additional DICOM attributes into header.

--- src/imagedata/formats/dicomplugin.py
+++ src/imagedata/formats/dicomplugin.py
@@ -128,28 +128,50 @@ class DICOMPlugin(AbstractPlugin):
                 - orientation
                 - imagePositions
                 - axes
+                - Modality, laterality, protocolName, bodyPartExamined, seriesDate, seriesTime, patientPosition
         """
-        hdr.studyInstanceUID = \
-            self.getDicomAttribute(dictionary, tag_for_keyword('StudyInstanceUID'))
-        hdr.studyID = \
-            self.getDicomAttribute(dictionary, tag_for_keyword('StudyID'))
-        hdr.seriesInstanceUID = \
-            self.getDicomAttribute(dictionary, tag_for_keyword('SeriesInstanceUID'))
-        frame_uid = self.getDicomAttribute(dictionary, tag_for_keyword('FrameOfReferenceUID'))
-        if frame_uid:
-            hdr.frameOfReferenceUID = frame_uid
-        hdr.SOPClassUID = self.getDicomAttribute(dictionary, tag_for_keyword('SOPClassUID'))
-        hdr.seriesNumber = self.getDicomAttribute(dictionary, tag_for_keyword('SeriesNumber'))
-        hdr.seriesDescription = self.getDicomAttribute(dictionary,
-                                                       tag_for_keyword('SeriesDescription'))
-        hdr.imageType = self.getDicomAttribute(dictionary, tag_for_keyword('ImageType'))
-
-        hdr.accessionNumber = self.getDicomAttribute(dictionary,
-                                                     tag_for_keyword('AccessionNumber'))
-        hdr.patientName = self.getDicomAttribute(dictionary, tag_for_keyword('PatientName'))
-        hdr.patientID = self.getDicomAttribute(dictionary, tag_for_keyword('PatientID'))
-        hdr.patientBirthDate = self.getDicomAttribute(dictionary,
-                                                      tag_for_keyword('PatientBirthDate'))
+        attributes = [
+            'patientName', 'patientID', 'patientBirthDate',
+            'studyInstanceUID', 'studyID',
+            'seriesInstanceUID', 'frameOfReferenceUID',
+            'seriesDate', 'seriesTime', 'seriesNumber', 'seriesDescription',
+            'imageType', 'accessionNumber',
+            'modality', 'laterality',
+            'protocolName', 'bodyPartExamined', 'patientPosition',
+            'SOPClassUID'
+        ]
+        for attribute in attributes:
+            dicom_attribute = attribute[0].upper() + attribute[1:]
+            setattr(hdr, attribute,
+                    self.getDicomAttribute(dictionary, tag_for_keyword(dicom_attribute)))
+        # hdr.studyInstanceUID = \
+        #     self.getDicomAttribute(dictionary, tag_for_keyword('StudyInstanceUID'))
+        # hdr.studyID = \
+        #     self.getDicomAttribute(dictionary, tag_for_keyword('StudyID'))
+        # hdr.seriesInstanceUID = \
+        #     self.getDicomAttribute(dictionary, tag_for_keyword('SeriesInstanceUID'))
+        # frame_uid = self.getDicomAttribute(dictionary, tag_for_keyword('FrameOfReferenceUID'))
+        # if frame_uid:
+        #     hdr.frameOfReferenceUID = frame_uid
+        # hdr.SOPClassUID = self.getDicomAttribute(dictionary, tag_for_keyword('SOPClassUID'))
+        # hdr.seriesDate = self.getDicomAttribute(dictionary, tag_for_keyword('SeriesDate'))
+        # hdr.seriesTime = self.getDicomAttribute(dictionary, tag_for_keyword('SeriesTime'))
+        # hdr.seriesNumber = self.getDicomAttribute(dictionary, tag_for_keyword('SeriesNumber'))
+        # hdr.seriesDescription = self.getDicomAttribute(dictionary,
+        #                                                tag_for_keyword('SeriesDescription'))
+        # hdr.imageType = self.getDicomAttribute(dictionary, tag_for_keyword('ImageType'))
+        #
+        # hdr.accessionNumber = self.getDicomAttribute(dictionary,
+        #                                              tag_for_keyword('AccessionNumber'))
+        # hdr.patientName = self.getDicomAttribute(dictionary, tag_for_keyword('PatientName'))
+        # hdr.patientID = self.getDicomAttribute(dictionary, tag_for_keyword('PatientID'))
+        # hdr.patientBirthDate = self.getDicomAttribute(dictionary,
+        #                                               tag_for_keyword('PatientBirthDate'))
+        # hdr.modality = self.getDicomAttribute(dictionary, tag_for_keyword('Modality'))
+        # hdr.laterality = self.getDicomAttribute(dictionary, tag_for_keyword('Laterality'))
+        # hdr.protocolName = self.getDicomAttribute(dictionary, tag_for_keyword('ProtocolName'))
+        # hdr.bodyPartExamined = self.getDicomAttribute(dictionary, tag_for_keyword('BodyPartExamined'))
+        # hdr.patientPosition = self.getDicomAttribute(dictionary, tag_for_keyword('PatientPosition'))
 
         hdr.spacing = self.__get_voxel_spacing(dictionary)
 
