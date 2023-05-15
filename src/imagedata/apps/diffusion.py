c6c49f257af41facdb1b1f9de8633e99a39fbd2f
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2023-05-15 11:51:16 +0200

    diffusion: Improved code structure.

--- src/imagedata/apps/diffusion.py
+++ src/imagedata/apps/diffusion.py
@@ -1,9 +1,8 @@
 """Extract diffusion MRI parameters.
 """
-
 # Copyright (c) 2013-2023 Erling Andersen, Haukeland University Hospital, Bergen, Norway
 
-
+import numpy as np
 import pandas as pd
 
 
@@ -18,6 +17,10 @@ def get_g_vectors(img):
         IndexError: when no gradient vector is present in dataset.
     """
 
+    def get_DICOM_g_vector(ds):
+        # Attempt to address standard DICOM attributes
+        return ds['DiffusionGradientOrientation'].value
+
     def get_Siemens_g_vector(ds):
         block = ds.private_block(0x0019, 'SIEMENS MR HEADER')
         return block[0x0e].value
@@ -26,40 +29,42 @@ def get_g_vectors(img):
         block = ds.private_block(0x0019, 'GEMS_ACQU_01')
         return [block[0xbb].value, block[0xbc].value, block[0xbd].value]
 
-    v = []
+    _v = []
     # Extract pydicom dataset for first slice and each tag
     _slice = 0
+    _dwi_weighted = False
     for tag in range(img.shape[0]):
-        G = [0, 0, 0]
-        ds = img.DicomHeaderDict[_slice][tag][2]
+        _b = get_b_value(img, tag)
+        _dwi_weighted = _dwi_weighted or not np.isnan(_b)
+        _G = [np.nan, np.nan, np.nan]
+        _ds = img.DicomHeaderDict[_slice][tag][2]
 
-        # Attempt to address standard DICOM attributes
-        if 'DiffusionGradientOrientation' in ds:
-            G = ds['DiffusionGradientOrientation'].value
-
-        # Special solutions for Siemens and GE systems
-        try:
-            G = get_Siemens_g_vector(ds)
-        except KeyError:
+        for _method in [get_DICOM_g_vector, get_Siemens_g_vector, get_GEMS_g_vector]:
             try:
-                G = get_GEMS_g_vector(ds)
+                _G = _method(_ds)
+                break
             except KeyError:
                 pass
 
-        v.append({'b': get_b_value(img, tag), 'z': G[2], 'y': G[1], 'x': G[0]})
-    return pd.DataFrame(v)
+        _v.append({'b': _b, 'z': _G[2], 'y': _G[1], 'x': _G[0]})
+    if _dwi_weighted:
+        return pd.DataFrame(_v)
+    else:
+        raise IndexError('No b values found in dataset')
 
 
 def get_b_value(img, tag=0):
     """Get diffusion b value
 
     Args:
-        img: Series object
+        img: Series object.
     Returns:
-        b: float
-    Raises:
-        IndexError: when no diffusion b value is present in dataset
+        float: b value. Returns NaN when no b value is present in dataset.
     """
+
+    def get_DICOM_b_value(ds):
+        # Attempt to address standard DICOM attribute
+        return ds['DiffusionBValue'].value
 
     def get_Siemens_b_value(ds):
         block = ds.private_block(0x0019, 'SIEMENS MR HEADER')
@@ -71,19 +76,12 @@ def get_b_value(img, tag=0):
 
     # Extract pydicom dataset for given slice and tag
     _slice = 0
-    ds = img.DicomHeaderDict[_slice][tag][2]
+    _ds = img.DicomHeaderDict[_slice][tag][2]
 
-    # Attempt to address standard DICOM attribute
-    if 'DiffusionBValue' in ds:
-        return ds['DiffusionBValue'].value
-
-    # Special solutions for Siemens and GE systems
-    try:
-        return float(get_Siemens_b_value(ds))
-    except KeyError:
+    for _method in [get_DICOM_b_value, get_Siemens_b_value, get_GEMS_b_value]:
         try:
-            return get_GEMS_b_value(ds)
+            return _method(_ds)
         except KeyError:
             pass
 
-    return 0.0
+    return np.nan
