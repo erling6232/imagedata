bf71441537404b8280c8c438dacce86c7c5b2885
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2023-12-20 08:14:22 +0100

    Construct build action.

--- tests/test_archive_file.py
+++ tests/test_archive_file.py
@@ -7,17 +7,16 @@ import os
 import unittest
 import argparse
 import tempfile
-from .context import imagedata
-import imagedata.cmdline
-import imagedata.readdata
-import imagedata.archives
-import imagedata.transports
+# from .context import imagedata
+import src.imagedata.cmdline as cmdline
+import src.imagedata.archives as archives
+import src.imagedata.transports as transports
 
 
 class TestArchiveFile(unittest.TestCase):
     def setUp(self):
         parser = argparse.ArgumentParser()
-        imagedata.cmdline.add_argparse_options(parser)
+        cmdline.add_argparse_options(parser)
 
         self.opts = parser.parse_args([])
         if len(self.opts.output_format) < 1:
@@ -26,16 +25,16 @@ class TestArchiveFile(unittest.TestCase):
     # @unittest.skip("skipping test_unknown_mimetype")
     def test_unknown_mimetype(self):
         try:
-            _ = imagedata.archives.find_mimetype_plugin(
+            _ = archives.find_mimetype_plugin(
                 'unknown',
                 'data',
                 'r')
-        except imagedata.archives.ArchivePluginNotFound:
+        except archives.ArchivePluginNotFound:
             pass
 
     # @unittest.skip("skipping test_mimetype")
     def test_mimetype(self):
-        _ = imagedata.archives.find_mimetype_plugin(
+        _ = archives.find_mimetype_plugin(
             '*',
             'data',
             'r')
@@ -43,16 +42,16 @@ class TestArchiveFile(unittest.TestCase):
     # @unittest.skip("skipping test_unknown_url")
     def test_unknown_url(self):
         try:
-            _ = imagedata.archives.find_mimetype_plugin(
+            _ = archives.find_mimetype_plugin(
                 '*',
                 'unknown',
                 'r')
-        except imagedata.transports.RootIsNotDirectory:
+        except transports.RootIsNotDirectory:
             pass
 
     # @unittest.skip("skipping test_read_one_file")
     def test_read_one_file(self):
-        archive = imagedata.archives.find_mimetype_plugin(
+        archive = archives.find_mimetype_plugin(
             None,
             '.',
             'r')
@@ -62,13 +61,13 @@ class TestArchiveFile(unittest.TestCase):
     # @unittest.skip("skipping test_new_archive")
     def test_new_archive(self):
         with tempfile.TemporaryDirectory() as d:
-            archive = imagedata.archives.find_mimetype_plugin(
+            archive = archives.find_mimetype_plugin(
                 '*',
                 d,
                 'w')
             with archive.open('test.txt', 'w') as f:
                 f.write(b'Hello world!')
-            read_archive = imagedata.archives.find_mimetype_plugin(
+            read_archive = archives.find_mimetype_plugin(
                 '*',
                 d,
                 'r')
