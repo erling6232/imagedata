f45137f48dd339f09f2d8daf21f1b56ffa79df9b
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2024-01-29 07:22:49 +0100

    Updated.

--- src/imagedata/archives/__init__.py
+++ src/imagedata/archives/__init__.py
@@ -1,22 +1,17 @@
-#!/usr/bin/env python3
-
 """This module provides plugins for various image archive formats.
 
-Standard plugins provides support for filesystem, tar, tar.gz, tar.bz2, gzip, zip.
+Standard plugins provide support for local filesystem and zip archives.
 """
 
-# Copyright (c) 2018 Erling Andersen, Haukeland University Hospital, Bergen, Norway
+# Copyright (c) 2018-2024 Erling Andersen, Haukeland University Hospital, Bergen, Norway
 
 import os.path
 import logging
 import urllib.parse
 import fnmatch
+from ..transports import Transport
 
 logger = logging.getLogger(__name__)
-
-
-# class NotImageError(Exception):
-#     pass
 
 
 class ArchivePluginNotFound(Exception):
@@ -39,7 +34,7 @@ def find_plugin(pfind, url, mode="r", opts=None):
     raise ArchivePluginNotFound("Plugin for image archive {} not found.".format(ptype))
 
 
-def find_mimetype_plugin(mimetype, url, mode="r", opts=None):
+def find_mimetype_plugin(mimetype, url, mode="r", read_directory_only=False, opts=None):
     """Return plugin for given file type."""
     if opts is None:
         opts = {}
@@ -52,19 +47,35 @@ def find_mimetype_plugin(mimetype, url, mode="r", opts=None):
     else:
         urldict = urllib.parse.urlsplit(url, scheme="file")
         _path = urldict.path if len(urldict.path) > 0 else urldict.netloc
-    if urldict.scheme == 'xnat':
-        mimetype = 'application/zip'
+    # if urldict.scheme == 'xnat':
+    #     mimetype = 'application/zip'
+    # if mimetype is None:
+    #     logger.debug("imagedata.archives.find_mimetype_plugin: filesystem")
+    #     return find_plugin('filesystem', url, mode, opts=opts)
+    transport = None
+    if urldict.scheme:
+        transport = Transport(
+            urldict.scheme,
+            netloc=urldict.netloc,
+            root=urldict.path,
+            mode=mode,
+            read_directory_only=read_directory_only)
     if mimetype is None:
-        logger.debug("imagedata.archives.find_mimetype_plugin: filesystem")
-        return find_plugin('filesystem', url, mode, opts=opts)
+        # Get any transport requirement for mimetype
+        try:
+            mimetype = transport.mimetype
+        except AttributeError:
+            pass
     for pname, ptype, pclass in plugins['archive']:
         logger.debug("imagedata.archive.find_mimetype_plugin: compare '{}' to {}".format(
             mimetype, pclass.mimetypes))
         if mimetype in pclass.mimetypes:
             logger.debug("imagedata.archives.find_mimetype_plugin: {}, mode: {}".format(
                 ptype, mode))
-            return pclass(url=url, mode=mode, opts=opts)
-    if os.path.isfile(_path):
+            return pclass(url=url, transport=transport, mode=mode, opts=opts)
+    # if os.path.isfile(_path):
+    # if os.path.exists(_path):
+    if urldict.scheme == "file":
         logger.debug("imagedata.archives.find_mimetype_plugin: filesystem")
         try:
             return find_plugin('filesystem', url, mode, opts=opts)
