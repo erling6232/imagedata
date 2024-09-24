f19f587a01e3a3ccdaacb3b67673f1d471a36830
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2024-09-24 07:27:22 +0200

    Testing

--- src/imagedata/archives/filesystemarchive.py
+++ src/imagedata/archives/filesystemarchive.py
@@ -88,8 +88,8 @@ class FilesystemArchive(AbstractArchive, ABC):
         else:
             urldict = urllib.parse.urlsplit(url, scheme="file")
             print('FilesystemArchive.__init__: url:', url, 'urldict:', urldict)
-            import unittest.TestCase
-            print('unittest:', unittest.TestCase.id())
+            import sys
+            print('FilesystemArchive.__init__: argv:', sys.argv[0])
             if os.name == 'nt' and \
                     fnmatch.fnmatch(urldict.netloc, '[A-Za-z]:\\*'):
                 self.__netloc = ''
