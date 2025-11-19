e50a16a3fcd237c0185bd6d39d6f90a6415f4d2f
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2025-11-19 12:53:23 +0100

    Testing action

--- src/imagedata/series.py
+++ src/imagedata/series.py
@@ -241,7 +241,7 @@ class Series(np.ndarray):
         if obj.shape != obj.header.shape:
             import sys
             print('__new__: obj.shape {} obj.header.shape {}'.format(obj.shape, obj.header.shape), file=sys.stderr)
-            print('__new__: ob  {} {}'.format(type(obj), obj.dtype), file=sys.stderr)
+            print('__new__: obj  {} {}'.format(type(obj), obj.dtype), file=sys.stderr)
         # assert obj.shape == obj.header.shape, "obj.shape {} != obj.header.shape {}".format(obj.shape, obj.header.shape)
         return obj
 
