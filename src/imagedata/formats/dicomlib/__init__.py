6eefa9be592210f7e6ad7f8bcb11cbbb63f9bfe6
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2026-03-17 09:13:01 +0100

    Set common anonymization rules.

---
+++ src/imagedata/formats/dicomlib/__init__.py
@@ -0,0 +1,14 @@
+anonymization_rules = {
+    'patientName': 'ANONYMOUS',
+    'patientID': 'ANONYMOUS',
+    'patientBirthDate': '00010101',
+    'patientSex': 'O',
+    'patientIdentityRemoved': 'YES',
+    'deidentificationMethod': 'imagedata',
+    'studyDate': '00010101',
+    'studyTime': None,
+    'studyDescription': '',
+    'studyID': '1',
+    'referringPhysicianName': '',
+}
+
